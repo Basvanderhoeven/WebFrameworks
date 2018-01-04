b96a12c443782aa3bc5029ba7b1641daef879195
Joren <jorenjamar@hotmail.com>
2018-01-05 00:18:09 +0100

    random museum toegevoegd

---
+++ src/app/bas/randomMuseum/randomMuseum.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "app-randomMuseum",
+    templateUrl : "./randomMuseum.component.html"
+})
+
+export class RandomMuseumComponent{
+
+}
