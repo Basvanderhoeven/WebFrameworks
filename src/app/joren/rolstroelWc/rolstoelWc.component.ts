9bea777e1769c7643b65879d7b2ab94cebd4585d
Joren <jorenjamar@hotmail.com>
2018-01-04 19:25:57 +0100

    wc rolstoel toont teveel rijen

--- src/app/joren/rolstroelWc/rolstoelWc.component.ts
+++ src/app/joren/rolstroelWc/rolstoelWc.component.ts
@@ -11,33 +11,34 @@ import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
 export class RolstoelWcComponent implements OnInit{
     data : IWcRolstoel[];
     wcLijst : IWcRolstoel[] = [];
-    wcIDs : number[] = []
+    wcIDs : number[] = [];
     
     constructor(private service : WcService){}
     
     ngOnInit(){
         this.service.getLijst().subscribe(result => this.data = this.MapResult(result));
-        console.log(this.data);
     }
 
     private MapResult(result : IWc) : IWcRolstoel[]{
         for(var i=0; i < result.data.length; i++){
             if(result.data[i].integraal_toegankelijk == "ja"){
                 var wc : IWcRolstoel = 
-            {
-                objectid : result.data[i].id,
-                straat : result.data[i].straat,
-                huisnummer : result.data[i].huisnummer,
-                postcode : result.data[i].postcode,
-                district : result.data[i].district,
-                betalend : result.data[i].betalend,
-                doelgroep : result.data[i].doelgroep
-            }
-            this.wcIDs.push(i);
-            this.wcLijst.push(wc);
+                {
+                    objectid : result.data[i].id,
+                    straat : result.data[i].straat,
+                    huisnummer : result.data[i].huisnummer,
+                    postcode : result.data[i].postcode,
+                    district : result.data[i].district,
+                    betalend : result.data[i].betalend,
+                    doelgroep : result.data[i].doelgroep
+                }
+                this.wcIDs.push(i);
+                this.wcLijst.push(wc);
             }
         } 
-        return this.wcLijst;   
+        console.log(this.wcLijst);
+        console.log(this.wcIDs); 
+        return this.wcLijst; 
     }
 }
 
